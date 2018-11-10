e0bada1ae60221af58d9d596fca00b8427022087
florian@subtl.fr <b_vQNVxyc8BJDQARsiMAI9BG4I-i7q2h>
2018-11-10 15:46:07 +0100

    Set QuickQR package metadata

---
+++ quickqr/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# QuickQR, get QR code using your clipboard from your system tray.
+# Copyright (C) 2018 flow.gunso@gmail.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+QUICKQR_ORGANISATION_NAME="flow.gunso@gmail.com"
+QUICKQR_APPLICATION_NAME="QuickQR"
+QUICKQR_HOMEPAGE_URL = "http://gitlab.com/flowgunso/QuickQR"
+QUICKQR_VERSION = "0.dev0"
+
