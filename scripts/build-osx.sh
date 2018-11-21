ee8da6c263b9c39230411aedd8a841bf275fe520
florian@subtl.fr <b_vQNVxyc8BJDQARsiMAI9BG4I-i7q2h>
2018-11-21 16:19:32 +0100

    Commit cross platform build scripts and requirements packages

---
+++ scripts/build-osx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install Python on OSX.
+wget "https://www.python.org/ftp/python/3.6.6/python-3.6.6-macosx10.9.pkg"
+sudo installer -pkg python-3.6.6-macosx10.9.pkg -target /
+sudo python3 -m ensurepip
+pip3 install --upgrade pip setuptools wheel
+pip3 install virtualenv
+python3 -m virtualenv -p python3 .
+source bin/activate
+
+pip3 install -r requirements.txt
+pip3 install -r scripts/requirements.txt
+pyinstaller picard.spec
